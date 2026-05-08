28e0ddee4dfde1a7510e1a57d43ccc85f09f6f53
ooitianbao <ooitianbao720@gmail.com>
2026-05-08 21:06:20 +0800

    Update data.php

--- data.php
+++ data.php
@@ -1,29 +1,17 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "westwoodbites";
 
-$file = "bookings.json";
+// 创建数据库连接
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// CREATE booking
-if($_SERVER["REQUEST_METHOD"] === "POST"){
-    $data = json_decode(file_get_contents($file), true);
-
-    $new = [
-        "name" => $_POST["name"],
-        "phone" => $_POST["phone"],
-        "date" => $_POST["date"],
-        "time" => $_POST["time"],
-        "people" => $_POST["people"]
-    ];
-
-    $data[] = $new;
-
-    file_put_contents($file, json_encode($data));
-
-    echo "Booking Successful!";
-    exit;
+// 检查连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
 }
 
-// READ booking
-if($_SERVER["REQUEST_METHOD"] === "GET"){
-    echo file_get_contents($file);
-}
+// 支持中文
+$conn->set_charset("utf8mb4");
 ?>
